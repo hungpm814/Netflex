23427c5876be8281c36d0187bd104ee0851b3c86
Cuong Pham Quoc <cuongpq114@gmail.com>
2025-07-03 22:00:03 +0700

    Add Film detail page with common info.

---
+++ app/src/main/java/com/example/netflex/viewModels/FilmDetailViewModel.java
@@ -0,0 +1,11 @@
+package com.example.netflex.viewModels;
+
+import com.example.netflex.model.Actor;
+import com.example.netflex.model.Film;
+
+import java.util.List;
+
+public class FilmDetailViewModel {
+    public Film film;
+    public List<Actor> actors;
+}
