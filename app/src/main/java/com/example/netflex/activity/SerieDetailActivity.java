cf8b58254732bd9d2a3eb0a32a2c2de73593bfa6
Cuong Pham Quoc <cuongpq114@gmail.com>
2025-07-23 15:57:01 +0700

    Fix trailer feature.

--- app/src/main/java/com/example/netflex/activity/SerieDetailActivity.java
+++ app/src/main/java/com/example/netflex/activity/SerieDetailActivity.java
@@ -1,11 +1,15 @@
 package com.example.netflex.activity;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
@@ -18,14 +22,18 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.netflex.APIServices.ApiClient;
+import com.example.netflex.APIServices.CommentAPIService;
 import com.example.netflex.APIServices.ReviewAPIService;
 import com.example.netflex.APIServices.SerieAPIService;
 import com.example.netflex.R;
+import com.example.netflex.adapter.CommentAdapter;
 import com.example.netflex.adapter.EpisodeAdapter;
+import com.example.netflex.model.Comment;
 import com.example.netflex.model.Episode;
 import com.example.netflex.model.Genre;
 import com.example.netflex.model.Serie;
 import com.example.netflex.requestAPI.auth.RatingRequest;
+import com.example.netflex.responseAPI.CommentListResponse;
 import com.example.netflex.utils.SharedPreferencesManager;
 import com.example.netflex.viewModels.ReviewEditModel;
 import com.example.netflex.responseAPI.ReviewResponse;
@@ -46,7 +54,7 @@ public class SerieDetailActivity extends AppCompatActivity {
     private static final String TAG = "SerieDetailActivity";
 
     private ImageView poster, btnBack;
-    private TextView title, textAbout, textYear, textEpisodes;
+    private TextView title, textAbout, textYear, textEpisodes, textNoComments;
     private TextView textGenres, textCountries, textActors;
     private RecyclerView recyclerEpisodes;
     private TextView textRating;
@@ -57,6 +65,15 @@ public class SerieDetailActivity extends AppCompatActivity {
     private String serieId;
 
     private SharedPreferencesManager prefsManager;
+    private CommentAPIService commentAPIService;
+    private List<Comment> comments = new ArrayList<>();
+    private int pageSize = 5;
+    private int page = 1;
+    private RecyclerView recyclerView;
+    private CommentAdapter commentAdapter;
+    private SharedPreferencesManager sharedPreferencesManager;
+    private Button btnLoadMore;
+    private String sort = "desc";
 
 
     @Override
@@ -84,16 +101,18 @@ public class SerieDetailActivity extends AppCompatActivity {
         layoutRateContent = findViewById(R.id.layoutRateContent);
         NestedScrollView nestedScrollView = findViewById(R.id.nestedScrollView);
         layoutRating.setOnClickListener(v -> {
-            boolean isVisible = ratingBarLike.getVisibility() == View.VISIBLE;
-            ratingBarLike.setVisibility(isVisible ? View.GONE : View.VISIBLE);
-            layoutRateContent.setVisibility(isVisible ? View.VISIBLE : View.GONE);
+            boolean isVisible = ratingBarLike.getVisibility() == VISIBLE;
+            ratingBarLike.setVisibility(isVisible ? GONE : VISIBLE);
+            layoutRateContent.setVisibility(isVisible ? VISIBLE : GONE);
         });
+        btnLoadMore = findViewById(R.id.btnLoadMore);
+        textNoComments = findViewById(R.id.no_comments);
 
         nestedScrollView.setOnTouchListener((v, event) -> {
             if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                if (ratingBarLike.getVisibility() == View.VISIBLE) {
-                    ratingBarLike.setVisibility(View.GONE);
-                    layoutRateContent.setVisibility(View.VISIBLE);
+                if (ratingBarLike.getVisibility() == VISIBLE) {
+                    ratingBarLike.setVisibility(GONE);
+                    layoutRateContent.setVisibility(VISIBLE);
                 }
             }
             return false;
@@ -106,6 +125,10 @@ public class SerieDetailActivity extends AppCompatActivity {
             Toast.makeText(this, "Serie ID is missing", Toast.LENGTH_SHORT).show();
             return;
         }
+
+        initServices();
+        initApiServices();
+        loadComments(serieId, page, sort);
 
         SerieAPIService apiService = ApiClient.getRetrofit().create(SerieAPIService.class);
         apiService.getSerieDetail(serieId).enqueue(new Callback<SerieDetailResponse>() {
@@ -136,7 +159,7 @@ public class SerieDetailActivity extends AppCompatActivity {
                         recyclerEpisodes.setAdapter(adapter);
                     } else {
                         textEpisodes.setText("Không tìm thấy tập phim nào");
-                        recyclerEpisodes.setVisibility(RecyclerView.GONE);
+                        recyclerEpisodes.setVisibility(GONE);
                     }
                 } else {
                     Toast.makeText(SerieDetailActivity.this, "Failed to load serie detail", Toast.LENGTH_SHORT).show();
@@ -161,6 +184,57 @@ public class SerieDetailActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void initServices(){
+        sharedPreferencesManager = new SharedPreferencesManager(this);
+    }
+
+    private void initApiServices(){
+        commentAPIService = ApiClient.getRetrofit().create(CommentAPIService.class);
+    }
+
+    private void loadComments(String serieId, int page, String sort) {
+        Call<CommentListResponse> call = commentAPIService.getComments(serieId, page, pageSize, sort);
+        call.enqueue(new Callback<>() {
+            @Override
+            public void onResponse(Call<CommentListResponse> call, Response<CommentListResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+
+                    List<Comment> newComments = response.body().comments;
+
+                    if (newComments.isEmpty()) {
+                        textNoComments.setVisibility(VISIBLE);
+                        return;
+                    }
+
+                    comments.addAll(newComments);
+                    setupCommentAdapter(comments);
+
+                    if (response.body().hasMore) {
+                        btnLoadMore.setVisibility(VISIBLE);
+                    } else {
+                        btnLoadMore.setVisibility(GONE);
+                    }
+                } else {
+
+                    Toast.makeText(SerieDetailActivity.this, "Unable to load comments", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<CommentListResponse> call, Throwable t) {
+                Log.e("API_ERROR", "Failed to fetch comments", t);
+                Toast.makeText(SerieDetailActivity.this, "Failed to fetch comments" + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setupCommentAdapter(List<Comment> comments) {
+        recyclerView = findViewById(R.id.recyclerComments);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        commentAdapter = new CommentAdapter(this, comments, sharedPreferencesManager.getUserId());
+        recyclerView.setAdapter(commentAdapter);
     }
 
     private boolean isLoggedIn() {
